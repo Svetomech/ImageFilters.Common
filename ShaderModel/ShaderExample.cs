892b6a86a43d5bdfe1c815ea7b69e7bdc7b9ac8b
Yuri <JK98V@Yandex.ru>
2018-01-17 01:48:16 +0300

    25% speedup + advanced MultiThread from 2 to 1024

---
+++ ShaderModel/ShaderExample.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace Svetomech.ImageFilters
+{
+    public class ShaderExample : ShaderModelFilter
+    {
+        public ShaderExample(Bitmap image) : base(image)
+        {            
+            // Использование доп. буффера равного по размеру буфферу изображения. 
+            // По окончании обработки он будет скопирован в буфер изображения.
+            // Замедляет работу на 30-45%
+            EnableBuffer = true;
+            
+            // Изображение будет обрабатываться одновременно 4 потоками
+            // Даёт прирост на 5-20%
+            MultiThread = MultiThreadType.Thread4;
+        }
+        
+        // Прирост скорости в зависимости от установленных флагов
+        // BF - buffer, MT - multi-thread
+        // [BF] [  ]  // 1.00
+        // [BF] [MT]  // 0.80 +20%
+        // [  ] [  ]  // 0.55 +45%
+        // [  ] [MT]  // 0.50 +50%
+
+        protected override unsafe void Technique(int x, int y)
+        {            
+            byte* pixel = refPixel(x, y, false);
+
+            buffer[x, y, A] = pixel[A];
+            buffer[x, y, R] = pixel[R];
+            buffer[x, y, G] = pixel[G];
+            buffer[x, y, B] = pixel[B];
+        }
+    }
+
+}
